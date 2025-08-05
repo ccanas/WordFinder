e259a5f34e2e121233fbd3d08205c2dbcfd874dd
CRISTIAN <cf.canas@gmail.com>
2025-08-05 19:44:18 -0300

    Add controller testing

---
+++ WordFinder.Tests/ApiTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace WordFinder.Api.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            builder.UseEnvironment("Testing");
+            // Optional: Replace services with mocks or test doubles if needed.
+        });
+    }
+}
